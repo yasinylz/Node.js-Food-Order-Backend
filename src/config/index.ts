3a5c055130d13763dc3db0ab8983b539d165d9ab
yasinylz <ysn.yldzzza@gmail.com>
2025-03-24 01:29:17 +0300

    proje bitirilmiştir

---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+export const MONGO_URI = 'mongodb+srv://yasin:107149@exampledb.vcn4tfr.mongodb.net/?retryWrites=true&w=majority&appName=ExampleDB';
+export const APP_SECRET = '6s84df6sd6fs'
+
+export const PORT = process.env.PORT || 8000;
